cc7876a7b066bfb5158366347b687fdb1d4db270
David Stichter <david.stichter3414@gmail.com>
2015-12-17 18:43:13 -0500

    Moved, studentInfoForm into a modal on main.  No longer need the json.js file.  Button to fill in the student area, button to display modal to add a new student

--- jscripts/studentFill.js
+++ jscripts/studentFill.js
@@ -3,24 +3,54 @@ $(document).ready(function(){
   var studentList = []
   fbase.on('value',function(snapshot){
     studentList[studentList.length] = snapshot.val()
-    console.log(studentList)
-    console.log(snapshot.val())
   });
   fbase.on("child_added", function(snapshot){
     studentList[studentList.length] = snapshot.val()
   });
-  fbase.on('value',function(){
-    for(i = 0; i < studentList.length; i++){
-      var newStudent = $('<div>').append($('<p>').html('Name: ' + studentList[i].name))
-      .append($('<p>').html('Email: ' + studentList[i].email))
-      .append($('<p>').html('github: ' + studentList[i].github))
-      .append($('<p>').html('linkdin: ' + studentList[i].linkdin))
-      .append($('<p>').html('stackoverflow: ' + studentList[i].stackoverflow))
-      .append($('<p>').html('portfolio: ' + studentList[i].portfolio))
-      .append($('<p>').html('facebook: ' + studentList[i].facebook))
-      .append($('<p>').html('twitter: ' + studentList[i].twitter))
-      .append($('<div>').html('<br>'))
-      $('#studentListTest').append(newStudent)
+
+  $('#studentFill').on('click',function(){
+    fbase.on('value',function(){
+      for(i = 0; i < studentList.length; i++){
+        var newStudent = $('<div>').append($('<p>').html('Name: ' + studentList[i].name))
+        .append($('<p>').html('Email: ' + studentList[i].email))
+        .append($('<p>').html('github: ' + studentList[i].github))
+        .append($('<p>').html('linkdin: ' + studentList[i].linkdin))
+        .append($('<p>').html('stackoverflow: ' + studentList[i].stackoverflow))
+        .append($('<p>').html('portfolio: ' + studentList[i].portfolio))
+        .append($('<p>').html('facebook: ' + studentList[i].facebook))
+        .append($('<p>').html('twitter: ' + studentList[i].twitter))
+        .append($('<div>').html('<br>'))
+        $('#studentListTest').append(newStudent)
+      }
+    });
+  });
+
+  $('#submitForm').on('click', function(e){
+    e.preventDefault()
+    var name = $('#name').val()
+    var email = $('#email').val()
+    var github = $('#github').val()
+    var linkdin = $('#linkdin').val()
+    var stackoverflow = $('#stackoverflow').val()
+    var portfolio = $('#portfolio').val()
+    var facebook = $('#facebook').val()
+    var twitter = $('#twitter').val()
+    var student = {
+      "name": name,
+      "email": email,
+      "github": github,
+      "linkdin": linkdin,
+      "stackoverflow": stackoverflow,
+      "portfolio": portfolio,
+      "facebook": facebook,
+      "twitter": twitter   
     }
+    fbase.push(student)
+    $('#studentFormModal').modal('hide')
+
+  });
+
+  $('#studentForm').on('click', function(){
+    $('#studentFormModal').modal('show')
   });
 });
