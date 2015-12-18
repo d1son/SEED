9d3a6b69491411537465fa0361f33ebaaf4f05e2
David Stichter <david.stichter3414@gmail.com>
2015-12-18 01:01:21 -0500

    changed the p tags to hyperlinks to github/linkedin/etc

--- jscripts/studentFill.js
+++ jscripts/studentFill.js
@@ -1,28 +1,25 @@
 $(document).ready(function(){
   var fbase = new Firebase('https://incandescent-heat-4625.firebaseio.com/');
   var studentList = []
-  fbase.on('value',function(snapshot){
-    studentList[studentList.length] = snapshot.val()
-  });
   fbase.on("child_added", function(snapshot){
     studentList[studentList.length] = snapshot.val()
   });
 
   $('#studentFill').on('click',function(){
-    fbase.on('value',function(){
       for(i = 0; i < studentList.length; i++){
-        var newStudent = $('<div>').append($('<p>').html('Name: ' + studentList[i].name))
+        var newStudent = $('<div>')
+        .append($('<p>').html('Name: ' + studentList[i].name))
         .append($('<p>').html('Email: ' + studentList[i].email))
-        .append($('<p>').html('github: ' + studentList[i].github))
-        .append($('<p>').html('linkdin: ' + studentList[i].linkdin))
-        .append($('<p>').html('stackoverflow: ' + studentList[i].stackoverflow))
-        .append($('<p>').html('portfolio: ' + studentList[i].portfolio))
-        .append($('<p>').html('facebook: ' + studentList[i].facebook))
-        .append($('<p>').html('twitter: ' + studentList[i].twitter))
+        .append($('<a>').html('github ').attr('href', studentList[i].github))
+        .append($('<a>').html('linkedin ').attr('href', studentList[i].linkedin))
+        .append($('<a>').html('stackoverflow ').attr('href', studentList[i].stackoverflow))
+        .append($('<a>').html('portfolio ').attr('href', studentList[i].portfolio))
+        .append($('<a>').html('facebook ').attr('href', studentList[i].facebook))
+        .append($('<a>').html('twitter ').attr('href', studentList[i].twitter))
         .append($('<div>').html('<br>'))
         $('#studentListTest').append(newStudent)
       }
-    });
+
   });
 
   $('#submitForm').on('click', function(e){
@@ -30,7 +27,7 @@ $(document).ready(function(){
     var name = $('#name').val()
     var email = $('#email').val()
     var github = $('#github').val()
-    var linkdin = $('#linkdin').val()
+    var linkedin = $('#linkedin').val()
     var stackoverflow = $('#stackoverflow').val()
     var portfolio = $('#portfolio').val()
     var facebook = $('#facebook').val()
@@ -39,7 +36,7 @@ $(document).ready(function(){
       "name": name,
       "email": email,
       "github": github,
-      "linkdin": linkdin,
+      "linkedin": linkedin,
       "stackoverflow": stackoverflow,
       "portfolio": portfolio,
       "facebook": facebook,
@@ -52,5 +49,8 @@ $(document).ready(function(){
 
   $('#studentForm').on('click', function(){
     $('#studentFormModal').modal('show')
+  });
+  $('#test').on('click', function(){
+    fbase.child('Student').push('show')
   });
 });
